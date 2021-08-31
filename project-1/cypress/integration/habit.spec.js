876bd9db1be6117cabc61f5f5edabc355c7328ca
JSmith989 <jordansmith989@gmail.com>
2021-08-31 16:01:49 -0500

    commit

--- project-1/cypress/integration/habit.spec.js
+++ project-1/cypress/integration/habit.spec.js
@@ -4,4 +4,19 @@ describe('habit dashboard', () => {
   beforeEach(() => {
     cy.visit('/habits');
   });
+
+  it('should display modal when add button clicked', () => {
+    // find the add button and click it to open modal
+    cy.contains('button', 'Add').click();
+
+    // this should be visible
+    cy.contains('Add a new habit').should('be.visible');
+  });
+
+  it('should display habit card when new habit is added', () => {
+    cy.get('#habit-add-btn').click();
+    cy.get("input[placeholder='Habit']").type('Drink a cup of water');
+    cy.contains('Save Changes').click();
+    cy.contains('Drink a cup of water').should('be.visible');
+  });
 });
